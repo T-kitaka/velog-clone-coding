0a1939215af28d70b798d28142ab54eccb750275
Jimin730 <dnddnd120@naver.com>
2025-03-14 16:31:20 +0900

    Create repository

---
+++ src/main/java/com/spring/velogproject/VelogCloneCodingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.velogproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelogCloneCodingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelogCloneCodingProjectApplication.class, args);
+	}
+
+}
